6224ab40709195a534baa1a2fdf2725285cb8eb5
Hafzali <Hafzali@AF-W18>
2018-11-27 14:45:20 +0430

    Add project files.

---
+++ BookCafe/Controllers/LanguageController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using System.Globalization;
+
+namespace BookCafe.Controllers
+{
+    public class LanguageController : Controller
+    {
+        // GET: Language
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Change(String Language)
+        {
+            if (Language != null)
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(Language);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(Language);
+            }
+            HttpCookie cookie = new HttpCookie("language");
+            cookie.Value = Language;
+            Response.Cookies.Add(cookie);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BookCafe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookCafe.Startup))]
+namespace BookCafe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
